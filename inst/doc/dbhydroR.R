56c80cb76c50bca63742b2dda667b37f948543bb
jsta <jstachelek@utexas.edu>
2016-04-04 14:44:47 -0400

    update vignette build

--- inst/doc/dbhydroR.R
+++ inst/doc/dbhydroR.R
@@ -2,81 +2,116 @@
 ### Encoding: UTF-8
 
 ###################################################
-### code chunk number 1: dbhydroR.Rnw:49-50
+### code chunk number 1: dbhydroR.Rnw:50-51
 ###################################################
-options(continue = "+", width = 50, useFancyQuotes = FALSE)
+options(width = 50, useFancyQuotes = FALSE)
 
 
 ###################################################
-### code chunk number 2: dbhydroR.Rnw:53-56 (eval = FALSE)
-###################################################
-## install.packages("B:\\restoration_sciences\\projects\\joe_stachelek
-##                  \\R\\dbhydro_0.1-1.zip",type="win.binary",
-##                  repos=NULL,dependencies=TRUE)
-
-
-###################################################
-### code chunk number 3: dbhydroR.Rnw:63-64
+### code chunk number 2: dbhydroR.Rnw:64-65
 ###################################################
 library(dbhydroR)
 
 
 ###################################################
-### code chunk number 4: dbhydroR.Rnw:73-74 (eval = FALSE)
+### code chunk number 3: dbhydroR.Rnw:74-75 (eval = FALSE)
 ###################################################
-## example(dbhydro_get)
+## example(getwq)
 
 
 ###################################################
-### code chunk number 5: dbhydroR.Rnw:80-82 (eval = FALSE)
+### code chunk number 4: dbhydroR.Rnw:81-83 (eval = FALSE)
 ###################################################
-## dbhydro_get(station_id="FLAB08", date_min="2011-03-01",
-##             date_max="2012-05-01",test_name="CHLOROPHYLLA-SALINE")
+## getwq(station_id = "FLAB08", date_min = "2011-03-01", 
+##       date_max = "2012-05-01", test_name = "CHLOROPHYLLA-SALINE")
 
 
 ###################################################
-### code chunk number 6: dbhydroR.Rnw:87-89 (eval = FALSE)
+### code chunk number 5: dbhydroR.Rnw:88-90 (eval = FALSE)
 ###################################################
-## dbhydro_get(station_id=c("FLAB08","FLAB09"), date_min="2011-03-01",
-##             date_max="2012-05-01",test_name="CHLOROPHYLLA-SALINE")
+## getwq(station_id = c("FLAB08","FLAB09"), date_min = "2011-03-01",
+##       date_max = "2012-05-01", test_name = "CHLOROPHYLLA-SALINE")
 
 
 ###################################################
-### code chunk number 7: dbhydroR.Rnw:94-96 (eval = FALSE)
+### code chunk number 6: dbhydroR.Rnw:95-97 (eval = FALSE)
 ###################################################
-## dbhydro_get(station_id=c("FLAB0%"), date_min="2011-03-01",
-##             date_max="2012-05-01",test_name="CHLOROPHYLLA-SALINE")
+## getwq(station_id = c("FLAB0%"), date_min = "2011-03-01", 
+##       date_max = "2012-05-01", test_name = "CHLOROPHYLLA-SALINE")
 
 
 ###################################################
-### code chunk number 8: dbhydroR.Rnw:101-104 (eval = FALSE)
+### code chunk number 7: dbhydroR.Rnw:102-105 (eval = FALSE)
 ###################################################
-## dbhydro_get(station_id=c("FLAB08","FLAB09"), date_min="2011-03-01",
-##             date_max="2012-05-01",test_name=c("CHLOROPHYLLA-SALINE",
-##                                               "SALINITY"))
+## getwq(station_id = c("FLAB08","FLAB09"), date_min = "2011-03-01",
+##       date_max = "2012-05-01", test_name = c("CHLOROPHYLLA-SALINE",
+##       "SALINITY"))
 
 
 ###################################################
-### code chunk number 9: dbhydroR.Rnw:111-114 (eval = FALSE)
+### code chunk number 8: dbhydroR.Rnw:112-115 (eval = FALSE)
 ###################################################
-## dbhydro_get(station_id="FLAB08", date_min="2011-03-01",
-##             date_max="2011-05-01",test_name="CHLOROPHYLLA-SALINE",
-##             raw=TRUE)
+## getwq(station_id = "FLAB08", date_min = "2011-03-01", 
+##       date_max = "2011-05-01", test_name = "CHLOROPHYLLA-SALINE",
+##       raw = TRUE)
 
 
 ###################################################
-### code chunk number 10: dbhydroR.Rnw:121-122 (eval = FALSE)
+### code chunk number 9: dbhydroR.Rnw:122-124 (eval = FALSE)
 ###################################################
-## example(dbhydro_plot)
+## getdbkey(stationid = "JBTS", category = "WEATHER", param = "WNDS",
+##          detail.level = "summary")
 
 
 ###################################################
-### code chunk number 11: dbhydroR.Rnw:127-132 (eval = FALSE)
+### code chunk number 10: dbhydroR.Rnw:129-131 (eval = FALSE)
 ###################################################
-## dt<-dbhydro_get(station_id=c("FLAB08","FLAB09"),
-##                 date_min="2011-03-01",date_max="2014-05-01",
-##                 test_name=c("CHLOROPHYLLA-SALINE","SALINITY"))
-##   
-## dbhydro_plot(dt,label="bottomleft",abb=3)
+## getdbkey(stationid = "JBTS", category = "WEATHER", param = "WNDS",
+##          freq = "DA", detail.level = "full")
+
+
+###################################################
+### code chunk number 11: dbhydroR.Rnw:136-139 (eval = FALSE)
+###################################################
+## getdbkey(stationid = "JBTS", category = "WEATHER", param = "WNDS",
+##          freq = "DA", stat = "MEAN", recorder = "CR10", agency = "WMD",
+##          detail.level = "dbkey")
+
+
+###################################################
+### code chunk number 12: dbhydroR.Rnw:144-146 (eval = FALSE)
+###################################################
+## gethydro(dbkey = "15081",
+##          date_min = "2013-01-01", date_max = "2013-02-02")
+
+
+###################################################
+### code chunk number 13: dbhydroR.Rnw:151-154 (eval = FALSE)
+###################################################
+## gethydro(date_min = "2013-01-01", date_max = "2013-02-02",
+##          stationid = "JBTS", category = "WEATHER", param = "WNDS",
+##          freq = "DA", stat = "MEAN", recorder = "CR10", agency = "WMD")
+
+
+###################################################
+### code chunk number 14: dbhydroR.Rnw:159-161 (eval = FALSE)
+###################################################
+## gethydro(dbkey = c("15081", "15069"), date_min = "2013-01-01",
+##          date_max = "2013-02-02")
+
+
+###################################################
+### code chunk number 15: dbhydroR.Rnw:164-167 (eval = FALSE)
+###################################################
+## gethydro(date_min = "2013-01-01", date_max = "2013-02-02",
+##          category = "WEATHER", stationid = c("JBTS", "MBTS"),
+##          param = "WNDS", freq = "DA", stat = "MEAN")
+
+
+###################################################
+### code chunk number 16: dbhydroR.Rnw:172-174 (eval = FALSE)
+###################################################
+## example(getdbkey)
+## example(gethydro)
 
 
