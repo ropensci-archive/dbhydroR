9075aadc2fca3a032dab613cdad1ad9bbf376623
jsta <jstachelek@utexas.edu>
2016-09-28 14:21:59 -0400

    add package level documentation

---
+++ R/dbhydoR-package.R
@@ -0,0 +1,24 @@
+#' @title dbhydroR
+#'
+#' @description dbhydroR is an R interace to the South Florida Water Management District's DBHYDRO database which holds over 35 million hydrologic and water quality records from the Florida Everglades and surrounding areas.
+#'
+#' @section Hydrologic Data:
+#' The \code{\link[dbhydroR]{get_hydro}} function provides the capability to return:
+#' \itemize{
+#'   \item weather data
+#'   \item surfacewater data
+#'   \item groundwater data
+#'   \item water-quality sonde data
+#' }
+#'  
+#' @section Water Quality Data:
+#' The \code{\link[dbhydroR]{get_wq}} function provides the capability to return:
+#' \itemize{
+#'   \item water quality data
+#' }
+#' 
+#' @name dbhydroR-package
+#' @aliases dbhydroR
+#' @docType package
+#' @keywords package
+NULL
