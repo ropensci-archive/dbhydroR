77de9509f06c1afb2d80cfecd832b562b590070c
jsta <jstachelek@utexas.edu>
2017-04-10 14:05:05 -0400

    add tests for on-the-fly dbkeys, #10

--- tests/testthat/test-gethydro.R
+++ tests/testthat/test-gethydro.R
@@ -1,13 +1,13 @@
 context("get_hydro")
 
-test_that("ge_thydro works", {
+test_that("get_hydro works", {
   skip_on_cran()
   
   expect_is(get_hydro(dbkey = "15081", date_min = "2013-01-01",
     date_max = "2013-02-02"), "data.frame")
   
-  get_hydro(dbkey = c("15081", "15069"), date_min = "2013-01-01",
-    date_max = "2013-02-02")
+  expect_is(get_hydro(dbkey = c("15081", "15069"), date_min = "2013-01-01",
+    date_max = "2013-02-02"), "data.frame")
   
 })
 
@@ -25,5 +25,20 @@ test_that("non-character dates are handled", {
   expect_error(get_hydro(dbkey = "15081", date_min = 1980-01-01,
     date_max = "1980-02-02"),
     "Enter dates as quote-wrapped character strings in YYYY-MM-DD format")
+  
+})
+
+test_that("get_hydro retrieves dbkeys on-the-fly", {
+  skip_on_cran()
+  
+  expect_is(ncol(get_hydro(stationid = "C-54", category = "GW",
+            freq = "DA", date_min = "1990-01-01", date_max = "1990-02-02", 
+            longest = TRUE)), 2) 
+            
+            
+  expect_is(ncol(get_hydro(stationid = c("C-54", "M-1083", "G-561"), 
+                           category = "GW", freq = "DA", 
+                           date_min = "1990-01-01", date_max = "2017-04-10", 
+                           longest = TRUE)), 4)
   
 })
